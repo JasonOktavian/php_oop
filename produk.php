0de370e75a501a915757881c78fe460b43d69a49
JasonOktavian <jasonoktavian30@gmail.com>
2023-03-01 11:24:00 +0700

    Menghapus 2 instance lalu menambahkan serta memanggil property

--- produk.php
+++ produk.php
@@ -1,8 +1,17 @@
 <?php
+//class produk
 class produk
 {
+    //Menambah Property
+    public $judul = "judul",
+        $penulis = "penulis",
+        $penerbit = "penerbit",
+        $harga = 0;
 }
 
+//instance dan memanggil property
 $produk1 = new produk();
-$produk2 = new produk();
-$produk3 = new produk();
+$produk1->judul = "Naruto";
+$produk1->penulis = "Masashi Kishimoto";
+$produk1->penerbit = "Shonen Jump";
+$produk1->harga = "30000";
