82731c93fba0da4a7c6d78ee8baa5afd4e7716b9
JasonOktavian <jasonoktavian30@gmail.com>
2023-03-01 11:39:55 +0700

    Menambahkan instance produk2 serta membuat method yang dapat menampilkan keterangan dari produk lalu memanggil method dalam class yang akan digunakan oleh instance

--- produk.php
+++ produk.php
@@ -7,6 +7,11 @@ class produk
         $penulis = "penulis",
         $penerbit = "penerbit",
         $harga = 0;
+
+    public function getLabel()
+    {
+        return "$this->judul, $this->penulis, $this->penerbit";
+    }
 }
 
 //instance dan memanggil property
@@ -15,3 +20,16 @@ $produk1->judul = "Naruto";
 $produk1->penulis = "Masashi Kishimoto";
 $produk1->penerbit = "Shonen Jump";
 $produk1->harga = "30000";
+
+
+//instance dan memanggil property
+$produk2 = new produk();
+$produk2->judul = "Persona";
+$produk2->penulis = "Katsura Hashino";
+$produk2->penerbit = "ATLUS";
+$produk2->harga = "700000";
+
+//pemanggilan method di dalam class
+echo "Komik : " . $produk1->getLabel();
+echo "<br>";
+echo "Game  : " . $produk2->getLabel();
